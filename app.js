77dc4f579e98698be21cd0d2caf6f4bbcd9dccd5
yolixtly <yolixtly_13@hotmail.com>
2016-06-06 17:13:53 -0400

    dynamic table with style

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 var app = angular.module('multiplyApp', []);
-app.controller('MultiplicationCtrl', function(){
+
+app.controller('MultiplicationCtrl', function($attrs){
 	var vm = this;
-	this.numbers = [1,2,3,4,5,6,7,8,9,10];
+	vm.numberLimit = $attrs.initialNumberLimit || 10; 
+
+	vm.numbers = function(){
+		var numbers = [];
+		for(var i =0; i < vm.numberLimit; i++){
+			numbers[i] = i + 1;
+		}
+		return numbers;
+	};
+
+	vm.compute = function(a, b){
+		return a * b;
+	}
 });
