f7502d20a655e36c1bfd76d24acaaeeb84a44264
yolixtly <yolixtly_13@hotmail.com>
2016-06-06 16:30:57 -0400

    hard coded table

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-angular.module('multiplyApp', []);
+var app = angular.module('multiplyApp', []);
+app.controller('MultiplicationCtrl', function(){
+	var vm = this;
+	this.numbers = [1,2,3,4,5,6,7,8,9,10];
+});
